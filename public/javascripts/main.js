e87b2fc88c47dd52eac9128d60886d95a10b24b9
atoris1192 <atoris1192@gmail.com>
2018-12-26 21:37:38 +0900

    step2.0

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,9 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const btn = document.querySelector('#btn')
 
   btn.addEventListener('click', () => {
-    const items = [ '大吉', '中吉', '末吉', '凶']
-    const random = Math.floor(Math.random()*items.length)
+    // const items = [ '大吉', '中吉', '末吉', '凶']
+    // const random = Math.floor(Math.random()*items.length)
+    // btn.textContent = items[random]
 
-    btn.textContent = items[random]
+    const url = 'http://localhost:3000/api/random'
+    fetch(url)
+      .then(res => {
+        return res.json()
+      })
+      .then(result => {
+        btn.textContent = result.item
+        console.log(result.item)
+      })
+      .catch(err => {
+        console.error(err)
+      })
   })
 })
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/api/random', (req, res, next) => {
+  const items = [ '大吉', '中吉', '末吉', '凶']
+  const random = Math.floor(Math.random()*items.length)
+  const item = {
+    item: items[random]
+  }
+  res.json(item)
+})
 
 module.exports = router;
