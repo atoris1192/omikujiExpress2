e0ce2a91e2e2f2fc1fbb612fac37ef084fb5e776
atoris1192 <atoris1192@gmail.com>
2018-12-26 14:37:15 +0900

    step1.0

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,13 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   'use strict'
-  console.log('-- main.js log --')
 
   const btn = document.querySelector('#btn')
 
   btn.addEventListener('click', () => {
-    console.log('-- btn click --')
+    const items = [ '大吉', '中吉', '末吉', '凶']
+    const random = Math.floor(Math.random()*items.length)
+
+    btn.textContent = items[random]
   })
 })
